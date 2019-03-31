4c5b4be2cbf035a0ebf80c11e61341229dd0b72a
11ray <raruidol@dsic.upv.es>
2019-03-31 20:05:41 +0200

    Errors with definitions fixed

--- Bloque3/AAA/exerciseEM.py
+++ Bloque3/AAA/exerciseEM.py
@@ -1,29 +1,30 @@
 import numpy as np
 import scipy as sp
-import seaborn as sns
 from matplotlib import pyplot as plt
 import scipy.stats as ss
 
+mu1 = -2
+mu2 = 2
+sigma = 2
+variance = 4
+n_m = 100
 
 class GMM(object):
     def __init__(self, X, k=2):
-        # dimension
         X = np.asarray(X)
         self.m, self.n = X.shape
         self.data = X.copy()
-        # number of mixtures
         self.k = k
 
     def _init(self):
         # init mixture means/sigmas
-        self.mean_arr = np.asmatrix(np.random.random((self.k, self.n)))
+        self.mean_arr = np.asmatrix(np.array([[mu1], [mu2]]))
         self.sigma_arr = np.array([np.asmatrix(np.identity(self.n)) for i in range(self.k)])
+        #self.sigma_arr = np.asmatrix(np.array([[2], [2]]))
         self.phi = np.ones(self.k) / self.k
         self.w = np.asmatrix(np.empty((self.m, self.k), dtype=float))
-        # print(self.mean_arr)
-        # print(self.sigma_arr)
 
-    def fit(self, tol=1e-4):
+    def fit(self, tol=0.0001):
         self._init()
         num_iters = 0
         ll = 1
@@ -45,7 +46,6 @@ class GMM(object):
         for i in range(self.m):
             tmp = 0
             for j in range(self.k):
-                # print(self.sigma_arr[j])
                 tmp += sp.stats.multivariate_normal.pdf(self.data[i, :],
                                                         self.mean_arr[j, :].A1,
                                                         self.sigma_arr[j, :]) * \
@@ -58,7 +58,6 @@ class GMM(object):
         self.m_step()
 
     def e_step(self):
-        # calculate w_j^{(i)}
         for i in range(self.m):
             den = 0
             for j in range(self.k):
@@ -81,28 +80,20 @@ class GMM(object):
                 _mu_j += (self.data[i, :] * self.w[i, j])
                 _sigma_j += self.w[i, j] * (
                             (self.data[i, :] - self.mean_arr[j, :]).T * (self.data[i, :] - self.mean_arr[j, :]))
-                # print((self.data[i, :] - self.mean_arr[j, :]).T * (self.data[i, :] - self.mean_arr[j, :]))
             self.mean_arr[j] = _mu_j / const
             self.sigma_arr[j] = _sigma_j / const
-        # print(self.sigma_arr)
 
+p = np.random.randint(0,n_m)
 
-mu1 = 2
-mu2 = 4
-sigma = 2
-variance = 4
-
-p = np.random.randint(0,100)
 print("Muestras: ", p)
 
 X1 = np.random.multivariate_normal([mu1], [[sigma]], p)
-X2 = np.random.multivariate_normal([mu2], [[sigma]], (100-p))
+X2 = np.random.multivariate_normal([mu2], [[sigma]], (n_m-p))
 X = np.vstack((X1, X2))
 print(X.shape)
 
-# Plot the data to which the GMM is being fitted
 plt.figure()
-
+plt.hist(X, density=True, bins="fd")
 plt.show()
 
 gmm = GMM(X)
@@ -111,14 +102,9 @@ mean_arr, sigma_arr, params = gmm.fit()
 n_components = params.shape[0]
 
 print("Num. Mixturas: ",n_components)
-
-# Weight of each component, in this case all of them are 1/3
-weights = np.array([p/100, (100-p)/100])
+weights = np.array([params[0], params[1]])
 print("Pesos(priori): ",weights)
-
-
-# Theoretical PDF plotting -- generate the x and y plotting positions
-xs = np.linspace(X.min(), X.max(), 1000)
+xs = np.linspace(X.min(), X.max(), 500)
 ys = np.zeros_like(xs)
 
 zipped = zip(mean_arr, params, weights)
